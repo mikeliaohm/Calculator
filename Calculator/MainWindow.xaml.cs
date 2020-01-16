5adc196b1732af976cf478f2a194962f084aeacd
Mike Liao <mike.hm.liao@gmail.com>
2020-01-16 23:31:21 +0800

    implemented simple method in constructors

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,10 +20,44 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double LastNumber, Result;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentButton.Click += PercentButton_Click;
+            equalButton.Click += EqualButton_Click;
+
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out LastNumber))
+            {
+                LastNumber = LastNumber / 100;
+            }
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out LastNumber))
+            {
+                LastNumber = LastNumber * -1;
+                resultLabel.Content = LastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
         }
 
         private void sevenButton_Click(object sender, RoutedEventArgs e)
