4f07ac1f8e6af314c0e0bc0932578fc35009cda3
Mike Liao <mike.hm.liao@gmail.com>
2020-01-16 23:11:36 +0800

    implemented seven button click event

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -23,6 +23,19 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "7";
+            }
+            else
+            {
+                resultLabel.Content += "7";
+            }
         }
     }
 }
